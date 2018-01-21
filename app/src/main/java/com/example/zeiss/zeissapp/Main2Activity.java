fec3c89a21583a218fd1380659e69306773ea380
Shashi Bhashkara <shashibhashkara@gmail.com>
2018-01-21 16:38:04 +0530

    Added functionality to buttons

--- app/src/main/java/com/example/zeiss/zeissapp/Main2Activity.java
+++ app/src/main/java/com/example/zeiss/zeissapp/Main2Activity.java
@@ -2,11 +2,13 @@ package com.example.zeiss.zeissapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.webkit.WebView;
 
 
 public class Main2Activity extends AppCompatActivity {
 
+    WebView webview;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,13 +18,31 @@ public class Main2Activity extends AppCompatActivity {
         //String data = getIntent().getExtras().getString("key");
 //        db.insertTemp(regId);
 
-        WebView webview = findViewById(R.id.webview);
-        webview.loadUrl("http://172.20.10.2");
+        webview = findViewById(R.id.webview);
+
+        webview.loadUrl("http://simranfarheen.com/raspberrypi.html");
+        webview.getSettings().setBuiltInZoomControls(true);
       /*Cursor r= db.getData(1);
        TextView display=(TextView)findViewById(R.id.showData);
        String show= r.getString(r.getColumnIndex(DatabaseHandler.TEMP_READINGS_VALUE));
        display.setText(show);
    */
 
+    }
+
+    public void down(View view){
+        webview.scrollTo(webview.getScrollX(), webview.getScrollY()+30);
+    }
+
+    public void up(View view){
+        webview.scrollTo(webview.getScrollX(), webview.getScrollY()-30);
+    }
+
+    public void left(View view){
+        webview.scrollTo(webview.getScrollX()-30, webview.getScrollY());
+    }
+
+    public void right(View view){
+        webview.scrollTo(webview.getScrollX()+30, webview.getScrollY());
     }
 }
